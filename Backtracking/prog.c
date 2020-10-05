62f7868a1198b25d8039d077638a1904c8e7afcd
Aman Agarwal <39858354+agarwalaman786@users.noreply.github.com>
2020-10-05 15:04:45 +0530

    Add files via upload

---
+++ Backtracking/prog.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	if(printf("I"))
+	switch(printf("love"))
+    while(printf("coding"))
+	return 0;
+}
---
+++ Backtracking/temp.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	/*int x = 10,y = 10,z =5,i;
+	i = x<y<z;
+	printf("%d\n",i);*/
+	char *ptr ="MyAnatomy";
+	*ptr = 'M';
+	printf("\n[%s]\n", ptr);
+	/*int a[5],i;
+	for(int i=1;i<=5;i++){
+		a[i] = i;
+		printf("\n%d",a[i]);
+	}*/
+
+}
